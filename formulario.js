08c363588b5fab37fa63399df1ae50508b4098c4
Luana Brunetti de Oliveira <162700949+luabrunetti@users.noreply.github.com>
2024-07-10 08:35:31 -0300

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -3,4 +3,10 @@ function Confirma() {
     let NomeDiretor = document.getElementById('diretormovie').value
     let FilmeFavorito = document.querySelector("Input[name='like-movie']:checked").value
     alert('O filme ' + Nomemovie + ' do diretor ' + NomeDiretor + ' foi marcado como ' + FilmeFavorito)  
+    
+    window.location.href='tela.html'
+}
+
+function Voltar() {
+    window.location.href='index.html'
 }
